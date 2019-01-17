7873f580baf3bba83b58460b6e3c52415dfa6730
Michael Culshaw-Maurer <mjculshawmaurer@ucdavis.edu>
2019-01-17 14:10:00 -0800

    I think it's a package now

---
+++ R/plotting_aesthetics.R
@@ -0,0 +1,17 @@
+#' A nice minimal ggplot theme
+#'
+#' @param base_family: the base font family, defaults to "Roboto Condensed"
+#'
+#' @return A ggplot theme
+#' @export
+#'
+#' @examples
+#' mtcars %>% ggplot2(aes(x = wt, y = mpg)) + geom_point() + minimal_ggplot_theme()
+minimal_ggplot_theme <- function(base_family = "Roboto Condensed"){
+  theme_bw(base_family = base_family) +
+    theme(legend.background = element_blank(), legend.key = element_blank(),
+          panel.background = element_blank(), panel.border = element_blank(),
+          strip.background = element_blank(), plot.background = element_blank(),
+          axis.line = element_blank(), panel.grid = element_blank(),
+          axis.ticks = element_blank())
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # MCMsBasics
 
 Convenience functions to help me get stuff done.
+
+## To Do
+1) Get a little more advanced with ggplot themes and color schemes. Should look at `hrbrthemes` for inspiration.
+2) Do some `brms` convenience functions
+  - especially look at converting my custom figure-generating functions
