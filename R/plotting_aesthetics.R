b0929da698a348b75f13e99df6a08c64e0dd6760
Michael Culshaw-Maurer <mjculshawmaurer@ucdavis.edu>
2020-11-17 16:51:42 -0800

    added dark mode to ggplot theme

--- R/plotting_aesthetics.R
+++ R/plotting_aesthetics.R
@@ -2,6 +2,7 @@
 #'
 #' @param base_family: the base font family, defaults to "Roboto Condensed"
 #' @param gridlines: should gridlines be displayed? defaults to F
+#' @param dark: use dark theme?
 #'
 #' @return A ggplot theme
 #' @export
@@ -9,7 +10,7 @@
 #'
 #' @examples
 #' mtcars %>% ggplot(aes(x = wt, y = mpg)) + geom_point() + minimal_ggplot_theme()
-minimal_ggplot_theme <- function(base_family = "Roboto Condensed", gridlines = F){
+minimal_ggplot_theme <- function(base_family = "Roboto Condensed", gridlines = F, dark = F){
   min_theme <- theme_bw(base_family = base_family) +
     theme(legend.background = element_blank(), legend.key = element_blank(),
           panel.background = element_blank(), panel.border = element_blank(),
@@ -19,6 +20,10 @@ minimal_ggplot_theme <- function(base_family = "Roboto Condensed", gridlines = F
 
   if(!gridlines){
     min_theme <- min_theme + theme(panel.grid = element_blank())
+  }
+
+  if(dark){
+    min_theme <- min_theme + theme(line = element_line(colour = "white"), text = element_text(colour = "white"), plot.background = element_rect(fill = "gray5"), axis.text = element_text(colour = "white"))
   }
 
   return(min_theme)
