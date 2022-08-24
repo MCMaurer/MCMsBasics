24c4c917c2333d853fac031e3f496b7a14a292e8
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2022-08-24 15:06:41 -0500

    fixed strip and axis titles

--- R/theme_mcm.R
+++ R/theme_mcm.R
@@ -18,65 +18,126 @@
 #'
 #' @examples
 #' mtcars %>% ggplot(aes(x = wt, y = mpg)) + geom_point() + theme_mcm()
-theme_mcm <- function (base_family = "fraunces", title_family = "fraunces",
-                       subtitle_family = "fraunces", axis_family = "fraunces",
-                       base_color = "gray10", primary_color = "white", accent_color = "black",
-                       base_size = 10, mult1 = 1.2, mult2 = 1.4, mult3 = 1.5, mult4 = 2.25,
-                       gridlines = F, facet_outlines = F, large_lineheight = F)
-{
-  a <- theme_bw() + theme(text = element_text(family = base_family,
-                                              colour = base_color, size = base_size),
-                          line = element_line(colour = base_color, size = 0.2),
-                          legend.background = element_blank(),
-                          legend.key = element_blank(),
-                          panel.background = element_blank(),
-                          panel.border = element_rect(fill = NA,
-                                                      colour = base_color, size = 0.2),
-                          strip.background = element_blank(),
-                          axis.line = element_blank(),
-                          panel.grid.minor = element_blank(),
-                          axis.ticks = element_blank(),
-                          plot.background = element_rect(fill = primary_color),
-                          axis.text = element_text(family = base_family,
-                                                   colour = base_color,
-                                                   size = base_size),
-                          plot.title = element_markdown(family = title_family,
-                                                    face = "bold.italic",
-                                                    colour = accent_color,
-                                                    size = base_size * mult4),
-                          axis.title = element_markdown(family = axis_family,
-                                                    colour = accent_color,
-                                                    size = base_size * mult2),
-                          strip.text = element_markdown(family = axis_family,
-                                                    colour = accent_color,
-                                                    size = base_size * mult2),
-                          legend.title = element_markdown(family = base_family,
-                                                      colour = base_color,
-                                                      size = base_size * mult2),
-                          legend.text = element_markdown(family = axis_family,
-                                                     colour = accent_color,
-                                                     size = base_size * mult1))
-  if (!gridlines) {
-    a <- a + theme(panel.grid = element_blank())
+theme_mcm <-
+  function (base_family = "fraunces",
+            title_family = "fraunces",
+            subtitle_family = "fraunces",
+            axis_family = "fraunces",
+            base_color = "gray10",
+            primary_color = "white",
+            accent_color = "black",
+            base_size = 10,
+            mult1 = 1.2,
+            mult2 = 1.4,
+            mult3 = 1.5,
+            mult4 = 2.25,
+            gridlines = F,
+            facet_outlines = F,
+            large_lineheight = F)
+  {
+    a <- theme_bw() + theme(
+      text = element_text(
+        family = base_family,
+        colour = base_color,
+        size = base_size
+      ),
+      line = element_line(colour = base_color, size = 0.2),
+      legend.background = element_blank(),
+      legend.key = element_blank(),
+      panel.background = element_blank(),
+      panel.border = element_rect(
+        fill = NA,
+        colour = base_color,
+        size = 0.2
+      ),
+      strip.background = element_blank(),
+      axis.line = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.ticks = element_blank(),
+      plot.background = element_rect(fill = primary_color),
+      axis.text = element_text(
+        family = base_family,
+        colour = base_color,
+        size = base_size
+      ),
+      plot.title = element_markdown(
+        family = title_family,
+        face = "bold.italic",
+        colour = accent_color,
+        size = base_size * mult4
+      ),
+      axis.title = element_markdown(
+        family = axis_family,
+        colour = accent_color,
+        size = base_size * mult2
+      ),
+      axis.title.x = element_markdown(
+        family = axis_family,
+        colour = accent_color,
+        size = base_size * mult2
+      ),
+      axis.title.y = element_markdown(
+        family = axis_family,
+        colour = accent_color,
+        size = base_size * mult2
+      ),
+      strip.text = element_markdown(
+        family = axis_family,
+        colour = accent_color,
+        size = base_size * mult2
+      ),
+      strip.text.x = element_markdown(
+        family = axis_family,
+        colour = accent_color,
+        size = base_size * mult2
+      ),
+      strip.text.y = element_markdown(
+        family = axis_family,
+        colour = accent_color,
+        size = base_size * mult2
+      ),
+      legend.title = element_markdown(
+        family = base_family,
+        colour = base_color,
+        size = base_size * mult2
+      ),
+      legend.text = element_markdown(
+        family = axis_family,
+        colour = accent_color,
+        size = base_size * mult1
+      )
+    )
+    if (!gridlines) {
+      a <- a + theme(panel.grid = element_blank())
+    }
+    if (!facet_outlines) {
+      a <- a + theme(panel.border = element_blank())
+    }
+    if (large_lineheight) {
+      a <-
+        a + theme(
+          plot.subtitle = element_markdown(
+            family = subtitle_family,
+            face = "italic",
+            colour = accent_color,
+            size = base_size * mult3,
+            lineheight = 1
+          )
+        )
+    }
+    else {
+      a <-
+        a + theme(
+          plot.subtitle = element_markdown(
+            family = subtitle_family,
+            face = "italic",
+            colour = accent_color,
+            size = base_size * mult3
+          )
+        )
+    }
+    return(a)
   }
-  if (!facet_outlines) {
-    a <- a + theme(panel.border = element_blank())
-  }
-  if (large_lineheight) {
-    a <- a + theme(plot.subtitle = element_markdown(family = subtitle_family,
-                                                face = "italic",
-                                                colour = accent_color,
-                                                size = base_size * mult3,
-                                                lineheight = 1))
-  }
-  else {
-    a <- a + theme(plot.subtitle = element_markdown(family = subtitle_family,
-                                                face = "italic",
-                                                colour = accent_color,
-                                                size = base_size * mult3))
-  }
-  return(a)
-}
 
 
 #' A dark ggplot theme for MCM's personal use
@@ -100,99 +161,125 @@ theme_mcm <- function (base_family = "fraunces", title_family = "fraunces",
 #'
 #' @examples
 #' mtcars %>% ggplot(aes(x = wt, y = mpg)) + geom_point() + theme_mcm_dark()
-theme_mcm_dark <- function(base_family = "fraunces", title_family = "fraunces",
-                           subtitle_family = "fraunces", axis_family = "fraunces",
-                           base_color = "white",
-                           primary_color = "grey10",
-                           accent_color = "grey90",
-                           gridlines = F,
-                           facet_outlines = F,
-                           base_size = 10,
-                           mult1 = 1.2,
-                           mult2 = 1.4,
-                           mult3 = 1.5,
-                           mult4 = 2.25,
-                           large_lineheight = F) {
-  a <- theme_bw() +
-    theme(
-      text = element_text(
-        family = base_family,
-        colour = base_color,
-        size = base_size
-      ),
-      line = element_line(
-        colour = base_color,
-        size = 0.2),
-      legend.background = element_blank(),
-      legend.key = element_blank(),
-      panel.background = element_blank(),
-      panel.border = element_rect(
-        fill = NA,
-        colour = base_color,
-        size = 0.2
-      ),
-      strip.background = element_blank(),
-      axis.line = element_blank(),
-      panel.grid.minor = element_blank(),
-      axis.ticks = element_blank(),
-      plot.background = element_rect(fill = primary_color, color = NA),
-      axis.text = element_text(
-        family = base_family,
-        colour = base_color,
-        size = base_size
-      ),
-      plot.title = element_markdown(
-        family = title_family,
-        face = "bold.italic",
-        colour = accent_color,
-        size = base_size * mult4
-      ),
-      axis.title = element_markdown(
-        family = axis_family,
-        colour = accent_color,
-        size = base_size * mult2
-      ),
-      strip.text = element_markdown(
-        family = axis_family,
-        colour = accent_color,
-        size = base_size * mult2
-      ),
-      legend.title = element_markdown(
-        family = axis_family,
-        colour = accent_color,
-        size = base_size * mult2
-      ),
-      legend.text = element_markdown(
-        family = base_family,
-        colour = base_color,
-        size = base_size * mult1
+theme_mcm_dark <-
+  function(base_family = "fraunces",
+           title_family = "fraunces",
+           subtitle_family = "fraunces",
+           axis_family = "fraunces",
+           base_color = "white",
+           primary_color = "grey10",
+           accent_color = "grey90",
+           gridlines = F,
+           facet_outlines = F,
+           base_size = 10,
+           mult1 = 1.2,
+           mult2 = 1.4,
+           mult3 = 1.5,
+           mult4 = 2.25,
+           large_lineheight = F) {
+    a <- theme_bw() +
+      theme(
+        text = element_text(
+          family = base_family,
+          colour = base_color,
+          size = base_size
+        ),
+        line = element_line(colour = base_color,
+                            size = 0.2),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(
+          fill = NA,
+          colour = base_color,
+          size = 0.2
+        ),
+        strip.background = element_blank(),
+        axis.line = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.ticks = element_blank(),
+        plot.background = element_rect(fill = primary_color, color = NA),
+        axis.text = element_text(
+          family = base_family,
+          colour = base_color,
+          size = base_size
+        ),
+        plot.title = element_markdown(
+          family = title_family,
+          face = "bold.italic",
+          colour = accent_color,
+          size = base_size * mult4
+        ),
+        axis.title = element_markdown(
+          family = axis_family,
+          colour = accent_color,
+          size = base_size * mult2
+        ),
+        axis.title.x = element_markdown(
+          family = axis_family,
+          colour = accent_color,
+          size = base_size * mult2
+        ),
+        axis.title.y = element_markdown(
+          family = axis_family,
+          colour = accent_color,
+          size = base_size * mult2
+        ),
+        strip.text = element_markdown(
+          family = axis_family,
+          colour = accent_color,
+          size = base_size * mult2
+        ),
+        strip.text.x = element_markdown(
+          family = axis_family,
+          colour = accent_color,
+          size = base_size * mult2
+        ),
+        strip.text.y = element_markdown(
+          family = axis_family,
+          colour = accent_color,
+          size = base_size * mult2
+        ),
+        legend.title = element_markdown(
+          family = axis_family,
+          colour = accent_color,
+          size = base_size * mult2
+        ),
+        legend.text = element_markdown(
+          family = base_family,
+          colour = base_color,
+          size = base_size * mult1
+        )
       )
-    )
 
-  if (!gridlines) {
-    a <- a + theme(panel.grid = element_blank())
+    if (!gridlines) {
+      a <- a + theme(panel.grid = element_blank())
+    }
+
+    if (!facet_outlines) {
+      a <- a + theme(panel.border = element_blank())
+    }
+
+    if (large_lineheight) {
+      a <- a + theme(
+        plot.subtitle = element_markdown(
+          family = subtitle_family,
+          face = "italic",
+          colour = accent_color,
+          size = base_size * mult3,
+          lineheight = 1
+        )
+      )
+    } else {
+      a <- a + theme(
+        plot.subtitle = element_markdown(
+          family = subtitle_family,
+          face = "italic",
+          colour = accent_color,
+          size = base_size * mult3
+        )
+      )
+    }
+
+    return(a)
   }
-
-  if (!facet_outlines) {
-    a <- a + theme(panel.border = element_blank())
-  }
-
-  if (large_lineheight){
-    a <- a + theme(plot.subtitle = element_markdown(
-      family = subtitle_family,
-      face = "italic",
-      colour = accent_color,
-      size = base_size * mult3,
-      lineheight = 1
-    ))
-  } else {
-    a <- a + theme(plot.subtitle = element_markdown(
-      family = subtitle_family,
-      face = "italic",
-      colour = accent_color,
-      size = base_size * mult3
-    ))
-  }
-
-  return(a)
-}
